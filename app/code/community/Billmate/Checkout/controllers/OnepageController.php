8a49d4270813f8752218e9786a615f2454af2c20
alex <a.grigorenko.belvg@gmail.com>
2019-04-12 14:24:49 +0300

    M1-5 [FEATURE] added check conflict rewrited controllers

--- app/code/community/Billmate/Checkout/controllers/OnepageController.php
+++ app/code/community/Billmate/Checkout/controllers/OnepageController.php
@@ -1,8 +1,14 @@
 <?php
-
 require_once Mage::getModuleDir('controllers', 'Mage_Checkout') . DS . 'OnepageController.php';
+$pathCustomPay = Mage::getModuleDir('controllers', 'Billmate_CustomPay') . DS . 'OnepageController.php';
+if (file_exists($pathCustomPay)) {
+    require_once $pathCustomPay;
+    abstract class Billmate_Checkout_Adapter extends Billmate_CustomPay_OnepageController{}
+} else {
+    abstract class Billmate_Checkout_Adapter extends Mage_Checkout_OnepageController{}
+}
 
-class Billmate_Checkout_OnepageController extends Mage_Checkout_OnepageController
+class Billmate_Checkout_OnepageController extends Billmate_Checkout_Adapter
 {
     /**
      * @var Billmate_Checkout_Helper_Data
